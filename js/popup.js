e4a2de0b920b7ced33d927f8e51bbeb1ab340935
bhargavat <bhargava.ramisetty@gmail.com>
2016-10-27 12:29:57 -0700

    Removed unused js file

--- js/popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var submit = document.getElementById('submit');
-  submit.addEventListener('click', function() {
-
-	chrome.tabs.query({active: true}, function(tab){
-		submitted();
-		});
-  }, false);
-}, false);
